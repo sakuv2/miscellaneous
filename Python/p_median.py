42a12468b6cf90496dececf78bb106f39a4181e3
shinki <shinki@shinki.local>
2016-04-26 03:35:52 +0900

    j=j+1だったのをj=iに修正

--- Python/p_median.py
+++ Python/p_median.py
@@ -3,10 +3,14 @@
 p-median問題における動的計画法
 """""""""""""""""""""""""""
 # define(テストデータ)
-w = [5, 1, 4, 2, 7, 3, 6, 3, 9, 5]
-l = [1, 2, 2, 1, 3, 7, 3, 2, 1]
+#w = [5, 1, 4, 2, 7, 3, 6, 3, 9, 5]
+#l = [1, 2, 2, 1, 3, 7, 3, 2, 1]
+w = [2,1,2,1,2]
+l = [2,1,2,2]
+#w = [2, 1, 2]
+#l = [2, 1]
 n = len(w)
-k = 4
+k = 2
 
 # d(iからjまでの距離)の計算
 # d = []
@@ -16,10 +20,12 @@ k = 4
 #     d.append([])
 #     for j in xrange(n):
 #         d[i].append(f(i, j))
-# 上記の処理を無理やり1行？で書いたもの↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+# 上記の処理を無理やり1行？で書いたもの ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 d = [(q.append([]), [(q[i].append(
     q[i][j - 1] + l[j - 1] if i < j else (0 if i == j else q[j][i])),
     q[i][j])[1] for j in xrange(n)])[1] for q in [[]] for i in xrange(n)]
+
+# 全探索アルゴリズム ==================================================================================
 
 
 def search_f(i, j, s):  # 順方向探索
@@ -29,11 +35,11 @@ def search_f(i, j, s):  # 順方向探索
     elif i >= n or j >= n:
         return 1e99
     # 再帰条件
-    if i <= j:  # 下に行くならi=j+1まで、右にいくならj=j+1まで
+    if i <= j:  # 下に行くならi=j+1まで、右にいくならj=j+1
         return min(search_f(j + 1, j, s - 1) + sum([w[x] * d[x][j] for x in xrange(i, j)]),
                    search_f(i, j + 1, s))
-    elif i > j:  # 下に行くならi=i+1まで、右にいくならj=j+1まで
-        return min(search_f(i + 1, j, s) + w[i] * d[i][j], search_f(i, j + 1, s))
+    elif i > j:  # 下に行くならi=i+1、右にいくならj=iまで
+        return min(search_f(i + 1, j, s) + w[i] * d[i][j], search_f(i, i, s))
 
 
 def search_b(i, j, s):  # 逆方向探索
@@ -46,10 +52,12 @@ def search_b(i, j, s):  # 逆方向探索
     if i >= j:  # 上に行くならi=j-1まで、左にいくならj=j-1
         return min(search_b(j - 1, j, s - 1) + sum([w[x] * d[x][j] for x in xrange(j + 1, i + 1)]),
                    search_b(i, j - 1, s))
-    elif i < j:  # 上に行くならi=i-1、左にいくならj=j-1
-        return min(search_b(i - 1, j, s) + w[i] * d[i][j], search_b(i, j - 1, s))
+    elif i < j:  # 上に行くならi=i-1、左にいくならj=iまで
+        return min(search_b(i - 1, j, s) + w[i] * d[i][j], search_b(i, i, s))
 
-# 漸化式を考える
+
+# 漸化式を考える =====================================================================================
 
 print search_f(0, 0, k)
 print search_b(n - 1, n - 1, k)
+
