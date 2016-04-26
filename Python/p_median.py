082f57c01f154fc3f41a32b26fe10c2cee7d9a3e
shinki <shinki@shinki.local>
2016-04-26 11:11:34 +0900

    debugする

--- Python/p_median.py
+++ Python/p_median.py
@@ -5,8 +5,8 @@ p-median問題における動的計画法
 # define(テストデータ)
 #w = [5, 1, 4, 2, 7, 3, 6, 3, 9, 5]
 #l = [1, 2, 2, 1, 3, 7, 3, 2, 1]
-w = [2,1,2,1,2]
-l = [2,1,2,2]
+w = [2, 1, 2, 1, 2]
+l = [2, 1, 2, 2]
 #w = [2, 1, 2]
 #l = [2, 1]
 n = len(w)
@@ -20,12 +20,12 @@ k = 2
 #     d.append([])
 #     for j in xrange(n):
 #         d[i].append(f(i, j))
-# 上記の処理を無理やり1行？で書いたもの ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+# 上記の処理を無理やり1行？で書いたもの ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 d = [(q.append([]), [(q[i].append(
     q[i][j - 1] + l[j - 1] if i < j else (0 if i == j else q[j][i])),
     q[i][j])[1] for j in xrange(n)])[1] for q in [[]] for i in xrange(n)]
 
-# 全探索アルゴリズム ==================================================================================
+# 全探索アルゴリズム ==============================================================
 
 
 def search_f(i, j, s):  # 順方向探索
@@ -56,8 +56,42 @@ def search_b(i, j, s):  # 逆方向探索
         return min(search_b(i - 1, j, s) + w[i] * d[i][j], search_b(i, i, s))
 
 
-# 漸化式を考える =====================================================================================
+# 漸化式を考える ================================================================
+
+# 行列を埋める
+dp = [[[None for s in xrange(k + 1)] for j in xrange(n + 1)]
+      for i in xrange(n + 1)]
+
+
+def DynamicPrograming():
+    for x in xrange(n + 1):
+        for s in xrange(k + 1):
+            dp[n][x][s] = 0
+            dp[x][n][s] = 1e99
+            
+    for i in reversed(xrange(n)):
+        for j in reversed(xrange(n)):
+            for s in xrange(k):
+                if i <= j:  # 上三角行列
+                    if s > 0:
+                        dp[i][j][s] = min(dp[j + 1][j][s - 1] +
+                                          sum([w[x] * d[x][j]
+                                               for x in xrange(i, j)]),
+                                          dp[i][j + 1][s])
+                    else:
+                        dp[i][j][s] = 1e99
+                elif i > j:  # 下三角行列
+                    if s > 0:  # まだkを減らせるなら
+                        print i, j, s, dp[i + 1][j][s]
+                        dp[i][j][s] = min(dp[i + 1][j][s] +
+                                          w[i] * d[i][j],
+                                          dp[j][j][s])
+                    else:
+                        dp[i][j][s] = sum([w[x] * d[x][j]
+                                           for x in xrange(i, n)])
+    return dp[0][0][k]
 
 print search_f(0, 0, k)
 print search_b(n - 1, n - 1, k)
-
+print DynamicPrograming()
+print dp
